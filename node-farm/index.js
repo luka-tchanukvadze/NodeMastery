8623515c2adb298d4d7dd8b68e5d8e61aa9f9ada
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-30 18:26:07 +0400

    async reaf write file with callbackhell

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,9 +1,24 @@
 const fs = require("fs");
 
-const text = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking, synchronous way
+// const text = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(text);
+// const textOut = `This is what we know about the avocado: ${text}\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log(text);
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!");
 
-const textOut = `This is what we know about the avocado: ${text}\nCreated on ${Date.now()}`;
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
 
-fs.writeFileSync("./txt/output.txt", textOut);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("file has been written");
+      });
+    });
+  });
+});
