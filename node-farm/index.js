24a9a7c42e783b5c8bfe1cc3510e01fad8144a9c
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-31 18:36:03 +0400

    created a simple api

--- node-farm/index.js
+++ node-farm/index.js
@@ -29,6 +29,9 @@ const url = require("url");
 
 //////////////////////////////////////////////////////////////// SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse();
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -37,12 +40,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("this is the product");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse();
-      res.writeHead(200, { "Content-type": "application/json" });
-
-      res.end(data);
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.end("Page not found!");
   }
