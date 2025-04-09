dc12bdcb733a60c5c6ad20d1b5dfe12256417488
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-09 22:49:27 +0400

    event loop test 2

--- event-loop/event-loop.js
+++ event-loop/event-loop.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finsihed"));
 
-fs.readFile("text-file-txt", () => {
+fs.readFile("test-file-txt", () => {
   console.log("I/O finished");
+
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finsihed"));
 });
 
 console.log("Hello from the top-level");
