7cd41b0d3b336d2a5e8d358b77ecc3279b6dd7d8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-09 22:58:08 +0400

    change size of threadpool

--- event-loop/event-loop.js
+++ event-loop/event-loop.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finsihed"));
@@ -14,6 +15,18 @@ fs.readFile("test-file-txt", () => {
   setImmediate(() => console.log("Immediate 2 finsihed"));
 
   process.nextTick(() => console.log("process.nextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
 
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "password encrypted");
