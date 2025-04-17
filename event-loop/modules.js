281da03bfacc0c4ce3a08236697f00545545f9e2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-17 17:13:46 +0400

    exports in practice

--- event-loop/modules.js
+++ event-loop/modules.js
@@ -1,7 +1,13 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
+// module.exports
 const C = require("./test-module-1");
-
 const calc1 = new C();
 console.log(calc1.add(3, 5));
+
+// exports
+// const cacl2 = require("./test-module-2");
+// console.log(cacl2.add(2, 5));
+const { add, multiply } = require("./test-module-2");
+console.log(add(2, 5));
