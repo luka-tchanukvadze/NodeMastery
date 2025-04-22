feba19c33c16904a1525abf5f8121c6e97c14d3d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-22 21:24:35 +0400

    The Problem with Callbacks Callback Hell

--- asyncJS/index.js
+++ asyncJS/index.js
@@ -9,6 +9,11 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
-      console.log(res.body);
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        console.log("random dog image fetched");
+      });
     });
 });
