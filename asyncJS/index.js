c37e0078619d92abb33dd3809dd4054f6e177b74
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-22 21:29:14 +0400

    from callback hell to promises

--- asyncJS/index.js
+++ asyncJS/index.js
@@ -8,12 +8,14 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile("dog-img.txt", res.body.message, (err) => {
         console.log("random dog image fetched");
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
