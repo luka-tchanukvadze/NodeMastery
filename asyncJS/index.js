9f6473d0ac77f3bf77b9fbcd930b5661f60aa6fc
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-22 21:21:27 +0400

    reading file and usying api with superagent

---
+++ asyncJS/index.js
@@ -0,0 +1,14 @@
+// https://dog.ceo/api/breed/hound/images/random
+
+const fs = require("fs");
+const superagent = require("superagent");
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      console.log(res.body);
+    });
+});
