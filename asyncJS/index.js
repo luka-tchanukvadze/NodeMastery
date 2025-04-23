8611b374ca3a7635c507d33c8c73fb1b06ed3c7a
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-23 20:40:47 +0400

    resolve and reject intigration

--- asyncJS/index.js
+++ asyncJS/index.js
@@ -6,6 +6,7 @@ const superagent = require("superagent");
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
       resolve(data);
     });
   });
