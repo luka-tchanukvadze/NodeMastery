5e502a6eac8a483d79490bf44d05d5924d7982d0
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-25 23:04:01 +0400

    routes

--- project/app.js
+++ project/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from the server side' });
 });
 
+app.post('/', (req, res) => {
+  res.send('You can post to this endpiont');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
