0be6726c00021be58c2f2acccde0ca1deb7e8917
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-27 17:49:26 +0400

    responding to url parameters

--- project/app.js
+++ project/app.js
@@ -20,15 +20,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: tours.length,
-  //   data: {
-  //     tour,
-  //   },
-  // });
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
 
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
