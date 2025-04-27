83b3238f633de33d6ebcf2f4a65efdea5eca3765
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-27 17:39:24 +0400

    handling post request

--- project/app.js
+++ project/app.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const express = require('express');
+const { Torus } = require('lucide-react');
 
 const app = express();
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -15,6 +18,33 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // res.status(200).json({
+  //   status: 'success',
+  //   results: tours.length,
+  //   data: {
+  //     tour,
+  //   },
+  // });
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
