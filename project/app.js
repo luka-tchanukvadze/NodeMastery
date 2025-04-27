4494eb481dce5173a132c271567d455eaf4f712a
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-27 17:27:26 +0400

    clean up

--- project/app.js
+++ project/app.js
@@ -3,14 +3,6 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from the server side' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpiont');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
