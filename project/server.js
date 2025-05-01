204973c447d0b8d559e09ca57b2ab20509d61bc2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-01 21:33:22 +0400

    environment variables

--- project/server.js
+++ project/server.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
 
 // console.log(process.env);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
