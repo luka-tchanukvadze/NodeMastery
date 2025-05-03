1819bdc8053a51dac58c5711a9fa8194eb0115a5
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-03 13:25:17 +0400

    testing the model

--- project/server.js
+++ project/server.js
@@ -39,6 +39,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error❤️', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
