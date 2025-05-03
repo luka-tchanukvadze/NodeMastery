882e1b48030fd2db52fc32a84ea17848c9cee474
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-03 13:47:03 +0400

    creating documents

--- project/controllers/tourController.js
+++ project/controllers/tourController.js
@@ -1,16 +1,5 @@
 const Tour = require('./../models/tour.Model');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Misssing name or price',
-    });
-  }
-
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -35,13 +24,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: null,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
