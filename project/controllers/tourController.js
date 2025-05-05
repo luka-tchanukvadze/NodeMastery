df66bb7c22f690f0532a02c7f05ebb490b124b61
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-05 21:50:12 +0400

    reading documents

--- project/controllers/tourController.js
+++ project/controllers/tourController.js
@@ -1,15 +1,23 @@
 const Tour = require('./../models/tour.Model');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
