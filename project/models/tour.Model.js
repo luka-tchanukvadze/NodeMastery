1e3c440be6cd95e6df76bf7a5a633cd7483ef845
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-06 14:31:20 +0400

    modelling the tour

--- project/models/tour.Model.js
+++ project/models/tour.Model.js
@@ -1,3 +1,4 @@
+const { trim } = require('lodash');
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
@@ -5,14 +6,36 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
   },
 });
 
