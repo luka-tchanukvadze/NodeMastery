5ab1a0aac1940d477d3b2827a184789af3f67a40
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-06 16:00:08 +0400

    filtering

--- project/controllers/tourController.js
+++ project/controllers/tourController.js
@@ -2,7 +2,24 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const tours = await Tour.find(queryObj);
 
     res.status(200).json({
       status: 'success',
