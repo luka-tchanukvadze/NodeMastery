93f0b8a07ca89c8c9d6e58ccc0cd8dd9dc217072
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-06 14:34:32 +0400

    modelling the tours

--- project/models/tour.Model.js
+++ project/models/tour.Model.js
@@ -36,7 +36,22 @@ const tourSchema = new mongoose.Schema({
   summary: {
     type: String,
     trim: true,
+    required: [true, 'A tour must have a description'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  imageCover: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
