3bfcb76e119775c3bb4f10b3a2cc4d9dd4486233
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-09 10:41:07 +0400

    document middleware

--- project/models/tourModel.js
+++ project/models/tourModel.js
@@ -1,5 +1,5 @@
-const { trim } = require('lodash');
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have duration'],
@@ -64,6 +65,19 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// Dcoument middleware: runs before .save() and create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
