a4c896336b653dc9797dc0f5e1bb2508e56aaf3e
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-14 17:08:47 +0400

    rrors During Development vs Production

--- project/controllers/errorController.js
+++ project/controllers/errorController.js
@@ -8,12 +8,24 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
+  // Operational, trused error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programmin or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('Error', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
