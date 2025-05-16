3bd78a190cb8dac2f0bb73ce1cc7c2b333a6483f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-16 20:51:40 +0400

    Catching Uncaught Exceptions

--- project/server.js
+++ project/server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -32,13 +38,3 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
-
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-console.log(x);
