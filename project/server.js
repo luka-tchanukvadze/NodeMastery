21536379aee3655d15b9dc15d3c3a93b138b980f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-16 20:42:27 +0400

    unhandled errors

--- project/server.js
+++ project/server.js
@@ -24,3 +24,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+});
