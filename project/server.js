98ac0f1fce89186589fde182db6869653b49e3b9
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-16 20:44:22 +0400

    Errors Outside Express Unhandled Rejections

--- project/server.js
+++ project/server.js
@@ -21,10 +21,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
 
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
