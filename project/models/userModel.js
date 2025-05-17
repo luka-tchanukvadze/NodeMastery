a83b9ec0cfeb604e6a16c01d527a3f6cedf1d8cc
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-17 21:23:34 +0400

    creating user model

---
+++ project/models/userModel.js
@@ -0,0 +1,32 @@
+const validator = require('validator');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail],
+  },
+  photo: {
+    type: String,
+    required: [true, 'User must have a photo'],
+  },
+  password: {
+    type: String,
+    required: [true, 'User must have a password'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'User must have a passwordConfirm'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
