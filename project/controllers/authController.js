d2fd159800ce8f388475057d8b0c0ac0bc2debbe
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-19 17:24:56 +0400

    loggin in ususers

--- project/controllers/authController.js
+++ project/controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -22,3 +23,21 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) Check if user exists && password is correct
+
+  // 3) If everything ok, send toekn to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
