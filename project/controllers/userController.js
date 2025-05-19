6ab386b0427134cade816b6dbcb807c23c2171a4
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-19 17:42:37 +0400

    loggin in users

--- project/controllers/userController.js
+++ project/controllers/userController.js
@@ -1,9 +1,21 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet definded',
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+  // res.status(500).json({
+  //   status: 'error',
+  //   message: 'This route is not yet definded',
+  // });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
