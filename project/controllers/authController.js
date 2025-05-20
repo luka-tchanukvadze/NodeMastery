8f9d668ae0802a0d70b3721a572dc4acd7ca3894
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-20 19:28:11 +0400

    protecting tour routes / plan

--- project/controllers/authController.js
+++ project/controllers/authController.js
@@ -51,3 +51,15 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
+});
