1514e42819b60675b240cdc8b67ce5be7cd54404
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-23 13:40:21 +0400

    Password Reset Functionality Setting New Password

--- project/controllers/authController.js
+++ project/controllers/authController.js
@@ -169,8 +169,8 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
   await user.save();
-  // 3) Update changedPasswordAt propery for the user
 
+  // 3) Update changedPasswordAt propery for the user
   // 4) Log the user in, send JWT
   const token = signToken(user._id);
 
