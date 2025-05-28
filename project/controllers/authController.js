cad4769c1ceb9e980f299c183cd89d33848b9979
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-28 17:25:42 +0400

    senidng JWT via cookie

--- project/controllers/authController.js
+++ project/controllers/authController.js
@@ -15,13 +15,19 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
-  res.cookie('jwt', token, {
+  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
-    secure: true,
     httpOnly: true,
-  });
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.cookie('jwt', token, cookieOptions);
 
   res.status(statusCode).json({
     status: 'success',
