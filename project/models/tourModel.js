25917586b2c06cc7eeb07cf37988ae6666b373b7
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-31 14:27:10 +0400

    Modelling Tour Guides Embedding

--- project/models/tourModel.js
+++ project/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -102,6 +103,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +118,13 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT middleware: runs before .save() and create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
 
   next();
 });
