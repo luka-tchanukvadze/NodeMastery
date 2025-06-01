71438c5ad05ec7b8216505fa719cae778d9a5e91
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-01 22:06:34 +0400

    populate

--- project/controllers/tourController.js
+++ project/controllers/tourController.js
@@ -36,7 +36,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   let id = req.params.id;
-  const tour = await Tour.findById(id).populate('guides');
+  const tour = await Tour.findById(id).populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
 
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
