22ac2d2f6614f70c927d74cacaef4354772c9453
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-04 17:49:25 +0400

    nested routes

--- project/routes/tourRoutes.js
+++ project/routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -24,6 +25,18 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+// POST /tour/432dfsa/reviews
+// GET /tour/432dfsa/reviews
+// GET /tour/432dfsa/reviews/fdw324df
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
