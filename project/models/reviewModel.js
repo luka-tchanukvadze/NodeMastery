b8b7ac54b0983e639e3e3173a6c043e007d1177a
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-12 17:06:38 +0400

    Calculating Average Rating on Tours

--- project/models/reviewModel.js
+++ project/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -68,6 +69,10 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
   ]);
 
   console.log(stats);
+  Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
 };
 
 reviewSchema.post('save', function (next) {
