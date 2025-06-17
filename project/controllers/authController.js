03a1f7357d2850bab33d4c6ae89ccb6c4c3571a2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-17 14:34:48 +0400

    Fixed Logout Controller

--- project/controllers/authController.js
+++ project/controllers/authController.js
@@ -69,7 +69,7 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = catchAsync(async (req, res, next) => {
-  req.cookie('jwt', 'loggedout', {
+  res.cookie('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
   });
